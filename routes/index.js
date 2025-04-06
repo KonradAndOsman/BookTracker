dbd0adea4ecdd6e05a4f6d99d0f10b01f084ef1e
KonradLisowski <vkonradlisowskiv@outlook.com>
2025-04-06 16:02:27 +0100

    testing fix for site 3

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+// to test if this fixes the website displaying properly
+const books = [
+  { id: 1, name: 'Sample Book 1', author: 'Author 1', year: 2021 },
+  { id: 2, name: 'Sample Book 2', author: 'Author 2', year: 2022 }
+];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: "BookTracker" });
+  res.render('index', { title: "BookTracker", books });
 });
-
 
 module.exports = router;
